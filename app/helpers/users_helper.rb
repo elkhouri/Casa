943d698e66f619399d211190e7f0d5cee9f31785
Alex Chen <elkhouri1@gmail.com>
2013-11-27 03:27:50 -0800

    Hover over child icons popover

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,14 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.name)
   end
   
+  def gravatar_thumbnail(user, options = { size: 100 })
+    gravatar_id = Digest::MD5::hexdigest(user.name)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm"
+    content = 'Grade: <br>Feedback:<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id ipsum ut orci cursus dictum. Ut dictum scelerisque interdum'
+    content_tag(:a, image_tag(gravatar_url, alt: user.name), 
+                class:'thumbnail', rel:'popover', title:user.name, :'data-content' => content,
+                href:user.id)
+  end
+  
 end
