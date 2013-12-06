37dcebb7155668e6dfc7ca17402e41c140cfd20d
Alex Chen <elkhouri1@gmail.com>
2013-12-06 01:49:47 -0800

    Fixed sign in/out popup for parent and student view

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,10 +36,16 @@ volunteer =  Volunteer.create!(name: 'Example Volunteer',
                         password: 'foobar',
                         password_confirmation: 'foobar')
 
+ids = []
+until ids.size == 50
+  n = rand.to_s[2..6]
+  ids.push(n) if not ids.include?(n)
+end
+
 10.times do |n|
   password  = "password"
   Parent.create!(name: Faker::Name.name,
-                  ID_num: rand.to_s[2..6],
+                  ID_num: ids.pop(),
                   phone: Faker::PhoneNumber.phone_number,
                   email: Faker::Internet.email,
                   address: Faker::Address.street_address,
@@ -49,7 +55,7 @@ end
 20.times do |n|
   password  = "password"
   Student.create!(name: Faker::Name.name,
-                  ID_num: rand.to_s[2..6],
+                  ID_num: ids.pop(),
                   grade: rand(1..12),
                   session: [:Tu, :Th, :TuTh].sample,
                   phone: Faker::PhoneNumber.phone_number,
@@ -61,7 +67,7 @@ end
 10.times do |n|
   password  = "password"
   Volunteer.create!(name: Faker::Name.name,
-                  ID_num: rand.to_s[2..6],
+                  ID_num: ids.pop(),
                   specialization: Faker::Company.catch_phrase,
                   interest: Faker::Company.bs,
                   session: [:Tu, :Th, :TuTh].sample,
@@ -81,4 +87,3 @@ students = Student.all
 children = students[0..5]
 children.each { |child| parent.parents!(child) }
 children.each { |child| volunteer.teach!(child) }
-
